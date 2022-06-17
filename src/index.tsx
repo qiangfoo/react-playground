3f318d05916509ab27355eafedc5679140df6298
jfu-sfdc <jfu@salesforce.com>
2022-06-17 15:06:31 +0000

    Add more packages

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App'
 
 const appElement = document.querySelector('#app')
-const root = createRoot(appElement)
-root.render(<App />)
+const root = createRoot(appElement!)
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
